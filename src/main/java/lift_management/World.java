4fe273677a73d65f45884c4d9439571db9c49e61
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-06 16:00:44 +0100

    Overriden World build();

--- src/main/java/lift_management/World.java
+++ src/main/java/lift_management/World.java
@@ -1,5 +1,6 @@
 package lift_management;
 
+import repast.simphony.context.Context;
 import sajas.core.Agent;
 import sajas.sim.repasts.RepastSLauncher;
 
@@ -9,6 +10,12 @@ import sajas.sim.repasts.RepastSLauncher;
 public class World extends RepastSLauncher {
     public static void main(String[] args) {
         return;
+    }
+
+    @Override
+    public Context build(Context<Object> context) {
+        super.build(context);
+        return context;
     }
 
     public String getName() {
