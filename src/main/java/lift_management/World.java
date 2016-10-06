b0beb435a43426719281ac19a70eb57086383b40
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-06 15:46:55 +0100

    Improved World stub.

--- src/main/java/lift_management/World.java
+++ src/main/java/lift_management/World.java
@@ -1,12 +1,21 @@
 package lift_management;
 
 import sajas.core.Agent;
+import sajas.sim.repasts.RepastSLauncher;
 
 /**
  * Created by Gustavo on 06/10/2016.
  */
-public class World extends Agent {
+public class World extends RepastSLauncher {
     public static void main(String[] args) {
         return;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    protected void launchJADE() {
+
     }
 }
