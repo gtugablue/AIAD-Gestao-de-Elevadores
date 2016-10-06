f15d93a1422ddd9f06571b8e72fa6cde5bbb251f
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-06 14:57:22 +0100

    Included SAJaS and created Lift and World class stubs.

---
+++ src/main/java/lift_management/World.java
@@ -0,0 +1,12 @@
+package lift_management;
+
+import sajas.core.Agent;
+
+/**
+ * Created by Gustavo on 06/10/2016.
+ */
+public class World extends Agent {
+    public static void main(String[] args) {
+        return;
+    }
+}
