ed294f37d8fac5c93331d805af12a974bb27547c
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-27 11:57:40 +0100

    Upgraded to Repast Simphony

---
+++ Lift Management/src/lift_management/LiftManagementLauncher.java
@@ -0,0 +1,36 @@
+package lift_management;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import sajas.core.Agent;
+import sajas.core.Runtime;
+import sajas.sim.repast3.Repast3Launcher;
+import sajas.sim.repasts.RepastSLauncher;
+import sajas.wrapper.ContainerController;
+
+/**
+ * Created by Gustavo on 06/10/2016.
+ */
+public class LiftManagementLauncher extends RepastSLauncher {
+    private ContainerController mainContainer;
+
+    public static void main(String[] args) {
+        return;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    protected void launchJADE() {
+        Runtime rt = Runtime.instance();
+        Profile p1 = new ProfileImpl();
+        mainContainer = rt.createMainContainer(p1);
+    }
+
+	@Override
+	public String[] getInitParam() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
