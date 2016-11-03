8174744c1c6672c9c05c0a5ec865068f1f3f8041
Gustavo Silva <silva95gustavo@gmail.com>
2016-11-03 18:40:39 +0000

    ...

--- Lift Management/src/lift_management/LiftManagementLauncher.java
+++ Lift Management/src/lift_management/LiftManagementLauncher.java
@@ -1,7 +1,21 @@
 package lift_management;
 
+import java.util.HashMap;
+
+import agents.Lift;
+import jade.core.AID;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
+import jade.wrapper.StaleProxyException;
+import repast.simphony.context.Context;
+import repast.simphony.context.Contexts;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.space.continuous.ContinuousAdder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.PointTranslator;
+import repast.simphony.space.continuous.SimpleCartesianAdder;
+import repast.simphony.space.continuous.StrictBorders;
 import sajas.core.Runtime;
 import sajas.sim.repasts.RepastSLauncher;
 import sajas.wrapper.ContainerController;
@@ -24,5 +38,21 @@ public class LiftManagementLauncher extends RepastSLauncher {
         Runtime rt = Runtime.instance();
         Profile p1 = new ProfileImpl();
         mainContainer = rt.createMainContainer(p1);
+        
+        try {
+			launchAgents();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
     }
+    
+    private void launchAgents() throws StaleProxyException {
+    	Context<Object> context = Contexts.createContext(Object.class, "context");
+    	ContinuousAdder<Object> adder = new SimpleCartesianAdder<Object>();
+    	PointTranslator translator = new StrictBorders();
+    	ContinuousSpaceFactory factory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+    	ContinuousSpace<Object> space = factory.createContinuousSpace("Building", context, adder, translator, 100.0, 100.0);
+    	Lift lift = new Lift(space);
+		mainContainer.acceptNewAgent("Lift", lift).start();
+	}
 }
