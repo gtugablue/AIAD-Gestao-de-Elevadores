ec2e2629fbbe4bee49636f532d49f458c46d8938
Gustavo Silva <silva95gustavo@gmail.com>
2016-11-13 01:37:35 +0000

    Created custom lift style.

---
+++ Lift Management/src/lift_management/agents/styles/LiftStyle.java
@@ -0,0 +1,12 @@
+package lift_management.agents.styles;
+
+import java.awt.Color;
+
+import repast.simphony.visualizationOGL2D.DefaultStyleOGL2D;
+
+public class LiftStyle extends DefaultStyleOGL2D {
+	@Override
+	public Color getColor(Object o) {
+		return Color.GRAY;
+	}
+}
