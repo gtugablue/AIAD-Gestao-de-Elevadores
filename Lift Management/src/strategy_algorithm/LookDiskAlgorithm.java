a76af8ce1973a9c5cacb00be81d8d8f0325e5c14
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2016-12-07 01:53:57 +0000

    Added lift weight to cost evaluation

--- Lift Management/src/strategy_algorithm/LookDiskAlgorithm.java
+++ Lift Management/src/strategy_algorithm/LookDiskAlgorithm.java
@@ -15,7 +15,7 @@ public class LookDiskAlgorithm {
 	 * @param requestedTask
 	 * @param maxBuildingFloor
 	 * @param currentPosition
-	 * @return Retorna a quantidade de pisos percorridos até parar o requestedFloor e atender o pedido do Human
+	 * @return Retorna a estimação de tempo que vai demorar até atender o pedido
 	 * @throws Exception 
 	 */
 	public static int evaluate(List<Pair<Integer, Task>> tasks, int requestedFloor, Task requestedTask, int maxBuildingFloor, int currentPosition) throws Exception{
@@ -29,11 +29,13 @@ public class LookDiskAlgorithm {
 		int previousStop = currentPosition;
 		Task previousTask = Task.STOP;
 		int floorsTraveled=0;
+		int numStops = 0;
 		boolean assigned = false;
 		
 		int nextStop;
 		Task nextTask;
 		Task direction;
+		
 		
 		//Percorrer todo o caminho do elevador e tentar encaixar a paragem no caminho
 		for(Pair<Integer, Task> task : tasks){
@@ -41,10 +43,11 @@ public class LookDiskAlgorithm {
 			nextTask = task.getValue();
 			direction = getDirection(tasks,tasks.indexOf(task), previousStop);
 			
-			if( floorInBetween(previousStop, nextStop, requestedFloor) && direction.equals(requestedTask) ){
+			if( floorInBetween(previousStop, nextStop, requestedFloor) && (direction.equals(requestedTask) || )){
 				
-				if(!previousTask.equals(direction)){	//Se a direção do elevador é diferente da direção de próxima tarefa, quer dizer que o elevador vai fazer uma paragem entre esta tarefa e a próxima tarefa que involve andar no sentido oposto.													
+				if(!(previousTask.equals(direction) || previousTask.equals(Task.STOP))){	//Se a direção do elevador é não for igual à direção de próxima tarefa e não for STOP, quer dizer que o elevador vai fazer uma paragem entre esta tarefa e a próxima tarefa que involve andar no sentido oposto.													
 					int estimatedDestiny = getEstimatedDestiny(previousStop, previousTask, maxBuildingFloor);
+					numStops++;
 					floorsTraveled += Math.abs( estimatedDestiny - previousStop) + Math.abs(estimatedDestiny + requestedFloor); //O custo será então igual a fazer uma viagem que satisfaça o pedido anterior mais o de ir dessa paragem para o requestedFloor
 				}
 				else{
@@ -54,6 +57,7 @@ public class LookDiskAlgorithm {
 				break;
 			}
 			
+			numStops++;
 			floorsTraveled += Math.abs(nextStop-previousStop);
 			previousStop = nextStop;
 			nextTask = previousTask;
@@ -63,10 +67,11 @@ public class LookDiskAlgorithm {
 		//Percorreu toda a lista de tasks e ainda assim não foi atribuido a uma posição
 		if(assigned == false){		
 			int estimatedDestiny = getEstimatedDestiny(previousStop, previousTask, maxBuildingFloor);
-			if(previousTask.equals(requestedTask) && floorInBetween(previousStop, estimatedDestiny, requestedFloor)){	//Se a direção do elevador é igual ao requestedTask e o requestedFloor está entre a última paragem e o destino quer dizer que o elevador pode parar no caminho para apanhar.	
+			if((previousTask.equals(requestedTask) && floorInBetween(previousStop, estimatedDestiny, requestedFloor)) || previousTask.equals(Task.STOP)){	//Se a direção do elevador é igual ao requestedTask e o requestedFloor está entre a última paragem e o destino quer dizer que o elevador pode parar no caminho para apanhar.	
 				floorsTraveled += Math.abs(previousStop - requestedFloor);
 			}else{
 				estimatedDestiny = getEstimatedDestiny(previousStop, previousTask, maxBuildingFloor);
+				numStops++;
 				floorsTraveled += Math.abs( estimatedDestiny - previousStop) + Math.abs(estimatedDestiny + requestedFloor); //O custo será então igual a fazer uma viagem que satisfaça o pedido anterior mais o de ir dessa paragem para o requestedFloor
 			}	
 		}
@@ -77,8 +82,10 @@ public class LookDiskAlgorithm {
 	protected static int getEstimatedDestiny(int previousStop, Task previousTask, int maxBuildingFloor) {
 		if(previousTask.equals(Task.DOWN)){
 			return 0;
-		}else{
+		}else if(previousTask.equals(Task.UP)){
 			return (int) Math.ceil((maxBuildingFloor+1 - previousStop)/2);
+		}else{
+			return previousStop;
 		}
 		
 	}
@@ -89,9 +96,11 @@ public class LookDiskAlgorithm {
 	
 	protected static Task getDirection(List<Pair<Integer, Task>> tasks,int i, int previousStop){		
 		Task direction;
+		
 		while(previousStop == tasks.get(i).getKey().intValue() && i < tasks.size()){
 			i++;
 		}
+		
 		int nextStop = tasks.get(i).getKey().intValue();
 		if(previousStop > nextStop) {
 			direction = Task.UP;
