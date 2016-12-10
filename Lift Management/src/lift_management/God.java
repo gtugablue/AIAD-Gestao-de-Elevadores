4c8dd923971901703cd7b8677dbf05a56b370715
Gustavo Silva <silva95gustavo@gmail.com>
2016-12-10 18:56:28 +0000

    Bugfix.

--- Lift Management/src/lift_management/God.java
+++ Lift Management/src/lift_management/God.java
@@ -139,10 +139,9 @@ public class God {
 		int currWeight = 0;
 		synchronized (this.humans) {
 			for (Human human : this.humans) {
-				/*if (!possibleDestinies[human.getDestinyFloor()]) {
-					// TODO recall
+				if (!possibleDestinies[human.getDestinyFloor()]) {
 					continue; // The lift destination is different from the human destination
-				}*/
+				}
 				if (human.getLiftID() != null)
 					continue; // Human already in a lift
 				
